a925c3fc763f9fa5d0c72e82bca2cc2cecb06987
JustTams <sarg.tier1@gmail.com>
2019-09-25 16:10:25 -0500

    creates a nextjs app instance to extend the header to every page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,2 +1,32 @@
+/**
+ * this is just the next js way of including the navbar on every component, similar to layout
+ */
+
 import React from 'react'
 import App, { Container as NextContainer } from 'next/app'
+import Head from 'next/head'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Header from '../components/navbar'
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <NextContainer>
+        <Head>
+          <title>Thoughts!</title>
+        </Head>
+        <Header />
+        <Container>
+          <Jumbotron>
+            <Component {...pageProps} />
+          </Jumbotron>
+        </Container>
+      </NextContainer>
+    )
+  }
+}
+
+export default MyApp
